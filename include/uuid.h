93bc62f059a67dbd7e4d3098d93418cf7738a419
Nick Richard <nick.richard@radwave.com>
2023-08-28 04:22:57 -0600

    Fix c++20 definition
    https://stackoverflow.com/a/53557835

--- include/uuid.h
+++ include/uuid.h
@@ -19,7 +19,7 @@
 
 #ifdef __cplusplus
 
-#  if (__cplusplus >= 202002L) || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)
+#  if (__cplusplus >= 201703L) || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)
 #    define LIBUUID_CPP20_OR_GREATER
 #  endif
 
